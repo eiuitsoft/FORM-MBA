ee095729653420f338100806f08ce9d0e050730f
tindinheiu <tin.doan@eiu.edu.vn>
2025-12-23 16:03:12 +0700

    #fix: change endpoint api check phone and passport

--- src/app/core/constants/api.const.ts
+++ src/app/core/constants/api.const.ts
@@ -7,9 +7,9 @@ export const BASE_API_URL = environment.API_URL;
 export const MBA_API = {
   ADD: `${BASE_API_URL}/MBAForm/Add`,
   CHECK_PASSPORT: (passportNo: string) =>
-    `${BASE_API_URL}/MBAForm/CheckPassport/${passportNo}`,
+    `${BASE_API_URL}/MBAForm/CheckPassport/check-passport/${passportNo}`,
   CHECK_MOBILE: (mobile: string) =>
-    `${BASE_API_URL}/MBAForm/CheckMobile/${mobile}`,
+    `${BASE_API_URL}/MBAForm/CheckMobile/check-mobile/${mobile}`,
   GET_ACTIVE_PROGRAMS: `${BASE_API_URL}/MBAProgram/GetActive`,
   GET_ACTIVE_LANGUAGES: `${BASE_API_URL}/MBALanguage/GetActive`,
   GET_ACTIVE_COUNTRIES: `${BASE_API_URL}/MBACountry/GetActive`,
