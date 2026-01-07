5d1f3060a204780567607935139288aebf78b477
xuanTungLeEIU <tung.lexuan@eiu.edu.vn>
2026-01-07 08:29:09 +0700

    #fix: update ESLint and Prettier configurations for improved code quality and formatting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
-export const trailingComma = "none";
+export const trailingComma = 'none';
 export const tabWidth = 2;
 export const singleQuote = true;
-export const printWidth = 80;
+export const printWidth = 120;
 export const semi = true;
 export const bracketSpacing = true;
-export const max_line_length = 100;
+export const max_line_length = 150;
+export const bracketSameLine = true;
